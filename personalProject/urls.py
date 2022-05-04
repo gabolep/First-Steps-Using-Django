90fa515f15385d7cdc482a99f8e71fc1be012ec0
gabolep <gabriel.escudero@alumnos.ucn.cl>
2022-05-04 18:06:42 -0400

    adding-views

--- personalProject/urls.py
+++ personalProject/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from petclub.views import HelloWorld
 
 urlpatterns = [
+    path('hi', HelloWorld.as_view(), name="helloworld"),
     path('admin/', admin.site.urls),
 ]
