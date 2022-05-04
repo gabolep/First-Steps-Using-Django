fff8ffc6c7dcf979df788a6943fb066daf3e882e
gabolep <gabriel.escudero@alumnos.ucn.cl>
2022-05-04 17:35:36 -0400

    adding-petclub-app-and-migrations

--- petclub/models.py
+++ petclub/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=255)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    rut = models.CharField(max_length=12)
+    def __str__(self):
+        return self.first_name
+class Pet(models.Model):
+    id = models.AutoField(primary_key=True)
+    species = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    color = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
